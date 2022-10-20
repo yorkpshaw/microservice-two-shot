e3cd2243be62a37c786657705e403659f3bac665
theresa <theresamargarettevilla@gmail.com>
2022-10-19 20:10:48 -0400

    merged York's work and mine

--- hats/api/hats_rest/acls.py
+++ hats/api/hats_rest/acls.py
@@ -2,13 +2,13 @@ import json
 import requests
 import os
 
-PEXELS_API_KEY = "563492ad6f9170000100000167984a3e6a3748e3912fc621273d7c5b"
+PEXELS_API_KEY = os.environ["PEXELS_API_KEY"]
 
 def get_hat(fabric, style, color):
     headers = {"Authorization": PEXELS_API_KEY}
     params = {
         "per_page": 1,
-        "query": f"hat {style} {color}",
+        "query": f"hat {fabric} {style} {color}",
     }
     url = "https://api.pexels.com/v1/search"
     response = requests.get(url, params=params, headers=headers)
